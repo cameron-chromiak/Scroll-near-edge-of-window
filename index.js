5b8d5b35103e0c4c69cb897bc3a06ffba5e7a212
cameron-chromiak <cam.chromiak@hotmail.com>
2019-05-31 17:44:44 -0400

    -drag to scroll, branch

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 $(document).ready(function(){
-document.addEventListener('mousemove', (e) =>{
-    let {clientX} = e
-    let {clientY} = e
+// document.addEventListener('mousemove', (e) =>{
+    // let {clientX} = e
+    // let {clientY} = e
     let width = window.innerWidth
     let height = window.innerHeight
+//
+//
+//     //area where mouse event will be trugered. 10% border around edge
+//     let leftBuffer = width * 0.1
+//     let rightBuffer = width - width * 0.1
+//     let topBuffer = height * 0.1
+//     let bottomBuffer = height - height * 0.1
+//
+//
+//     //chnage += -= to 20 to change speed.
+//     let scrollPosX = window.scrollX
+//     let scrollPosY = window.scrollY
+//
+//       if(clientX <= leftBuffer){
+//         scrollPosX -=width
+//         window.scrollTo(scrollPosX ,scrollPosY )
+//       }
+//       if(clientX >= rightBuffer){
+//         scrollPosX +=width
+//         window.scrollTo(scrollPosX ,scrollPosY )
+//     }
+//       if(clientY <= topBuffer){
+//         scrollPosY -= height
+//         window.scrollTo(scrollPosX ,scrollPosY )
+//       }
+//       if(clientY >= bottomBuffer){
+//         scrollPosY += height
+//         window.scrollTo(scrollPosX ,scrollPosY )
+//     }
+//   })
+//
 
-
-    //area where mouse event will be trugered. 10% border around edge
-    let leftBuffer = width * 0.1
-    let rightBuffer = width - width * 0.1
-    let topBuffer = height * 0.1
-    let bottomBuffer = height - height * 0.1
-
-
-    //chnage += -= to 20 to change speed.
-    let scrollPosX = window.scrollX
-    let scrollPosY = window.scrollY
-
-      if(clientX <= leftBuffer){
-        scrollPosX -=width
-        window.scrollTo(scrollPosX ,scrollPosY )
-      }
-      if(clientX >= rightBuffer){
-        scrollPosX +=width
-        window.scrollTo(scrollPosX ,scrollPosY )
-    }
-      if(clientY <= topBuffer){
-        scrollPosY -= height
-        window.scrollTo(scrollPosX ,scrollPosY )
-      }
-      if(clientY >= bottomBuffer){
-        scrollPosY += height
-        window.scrollTo(scrollPosX ,scrollPosY )
-    }
-  })
-
-
-
-
+//
+//
   let topLeft = document.querySelector('#top-left')
   let topRight = document.querySelector('#top-right')
   let bottomLeft = document.querySelector('#bottom-left')
@@ -45,24 +45,38 @@ document.addEventListener('mousemove', (e) =>{
   let sections = document.querySelectorAll('.section')
   let mainBtn  = document.querySelector('#main-btn')
   let container  = document.querySelector('#container')
+//
+//
+//   mainBtn.addEventListener('click', function(){
+//     toggleResize()
+//   })
+//
+//   function toggleResize(){
+//     sections.forEach( section => section.classList.toggle('section-shrink'))
+//     container.classList.toggle('container-sm')
+//   }
+//
+//
+//
 
+  let isMouseClicked = false;
 
-  mainBtn.addEventListener('click', function(){
-    toggleResize()
+  container.addEventListener('mousedown',function(e){
+    isMouseClicked = true
+    console.log(isMouseClicked)
+  })
+  container.addEventListener('mouseup',function(e){
+    isMouseClicked = false
+    console.log(isMouseClicked)
   })
 
-  function toggleResize(){
-    sections.forEach( section => section.classList.toggle('section-shrink'))
-    container.classList.toggle('container-sm')
-  }
 
-  sections.forEach( section =>{
-    section.addEventListener('click', function(e){
-      // if(!bigscreen)
-      toggleResize()
-      window.scrollTo(900, 800)
-    })
+    document.addEventListener('mousemove', function(e){
+        if(isMouseClicked){
+      console.log(e.pageX);
+    }
   })
+
 
 
 })
